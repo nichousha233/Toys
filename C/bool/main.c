3a0e48f113fb204d3f9d534423c78d741edbfc41
Yu Zhang <zhangyu31110@outlook.com>
2024-03-05 14:27:31 +0800

    fp16 and bool

---
+++ C/bool/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+    int a, b;
+    a = 1;
+    b = 1;
+    printf("a == b = %d\n", a == b);
+}
---
+++ C/fp16/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+	__fp16 a;
+	__fp16 b;
+	a = 1.2;
+	b = 3.4;
+	printf("a + b = %f\n", a + b);
+	return(0);
+}
