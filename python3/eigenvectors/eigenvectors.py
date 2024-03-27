a04a62ec238d7251435dc8c20ab89d301ce831b4
Yu Zhang <zhangyu31110@outlook.com>
2024-03-27 14:53:54 +0800

    just commit

--- python3/eigenvectors/eigenvectors.py
+++ python3/eigenvectors/eigenvectors.py
@@ -1,19 +1,55 @@
-#!/usr/local/bin/python3.9
+#!/usr/bin/python3
 import numpy as np
 #from numpy import linalg as LA
 from scipy.linalg import eigh
 
-N = 1600
-for i in range(6):
-    a = np.loadtxt('DEBUG1/a{}/rank0a.txt'.format(i)).reshape((N,N))
-    b = np.loadtxt('DEBUG1/b{}/rank0b.txt'.format(i)).reshape((N,N))
-    z = np.loadtxt('DEBUG1/z{}/rank0z.txt'.format(i)).reshape((N,N))
+N = 20
+for i in range(1):
 
-    w, v = eigh(a,b)
-    diff = v[:,0] - z[:,0]
-    add = v[:,0] + z[:,0]
-    np.savetxt('w{}.txt'.format(i), w)
-    np.savetxt('diff{}.txt'.format(i), diff)
-    np.savetxt('add{}.txt'.format(i), add)
-    np.savetxt('z0{}.txt'.format(i), z[:,0])
-    np.savetxt('v0{}.txt'.format(i), v[:,0])
+    a = np.loadtxt("Hp_Ek0/rank0")[:,1].reshape((N,N)).T
+    b = np.loadtxt("Mp_Ek0/rank0")[:,1].reshape((N,N)).T
+    z = np.loadtxt("Z_Ek0/rank0")[:,1].reshape((N,N)).T
+    w = np.loadtxt("W_Ek0/rank0")[:,1].reshape((N))
+    b = np.triu(b,1).T + np.triu(b,0)
+    a = np.triu(a,1).T + np.triu(a,0)
+
+    l, v = eigh(a,b, lower = False)
+
+    max_diff_l_w = np.max(np.abs(l-w))
+    vbv = v.T @ b @ v
+    zbz = z.T @ b @ z
+    vbv_I = vbv - np.identity(N)
+    zbz_I = zbz - np.identity(N)
+    print("max_diff_l_w = {}".format(max_diff_l_w))
+    print(np.trace(vbv), np.trace(zbz))
+    print(np.max(np.abs(vbv_I)), np.max(np.abs(zbz_I)))
+    print('')
+
+    #psi = np.loadtxt("Yorb_BLCYC_Ek0/rank0")[:,1].reshape((1000,N))
+    psi = np.loadtxt("v_tilde_f_Ek0/rank0")[:,1].reshape((N, 1000)).T
+    psiz = psi @ z
+    print(psiz.shape)
+    psiz2 = psiz * psiz
+    print(np.sum(psiz2[:,:]))
+    print('')
+
+    #psi = np.loadtxt("Yorb_BLCYC_Ek0/rank0")[:,1].reshape((1000,N))
+    psi = np.loadtxt("v_tilde_f_Ek0/rank0")[:,1].reshape((N, 1000)).T
+    psiv = psi @ v
+    print(psiv.shape)
+    psiv2 = psiv * psiv
+    print(np.sum(psiv2[:,:]))
+    print('')
+
+    psiz_out = np.loadtxt("v_tilde2_Ek0/rank0")[:,1].reshape((N, 1000)).T
+    psiz_out2 = psiz_out * psiz_out
+    print(np.sum(psiz_out2[:,:]))
+    print('')
+
+    psiQ = np.loadtxt("PsiQ/rank0")[:,1].reshape((N, 1000)).T
+    psiQ2 = psiQ * psiQ
+
+    div = psiQ - psiz
+    print(np.max(np.abs(div)))
+    print(np.sum(psiQ2))
+    print(np.sum(psiz_out2))
